821d2056c5054c152c66eb8fe2b982a7cde209cb
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-05-24 17:45:51 -0700

    add counter

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{count}</Text>
+      <View style={styles.buttons}>
+        <Button title='+1' onPress={() => setCount(count + 1)} />
+        <Button title='+5' onPress={() => setCount(count + 5)} />
+      </View>
+
+
     </View>
   );
 }
@@ -12,8 +20,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#c3e5e8',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    margin: 20,
+  }
 });
